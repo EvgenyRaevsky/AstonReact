1892cc4c5e4b77e45c08a5025231e7f79fc392dc
Evgeny <zhenya.raewskiy@yandex.ru>
2024-03-12 14:18:48 +0300

    adding the main page and connecting the routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+
 export const App = () => {
   return (
-    <div></div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
