3374a7eff265692ae33da93928364f1cf7909ec7
Evgeny <zhenya.raewskiy@yandex.ru>
2024-03-12 13:27:56 +0300

    creating project

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:react/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    plugins: [
+        "react"
+    ],
+    rules: {
+        "no-console": "warn",
+        "import/prefer-default-export": "off",
+        "react/jsx-no-useless-fragment": "warn",
+        "react/react-in-jsx-scope": "off",
+        "no-unused-expressions": "error",
+        "react/prop-types": "off",
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
