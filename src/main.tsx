03c7081e5965e606bc1e11b7272e2039ef97eaf9
Evgeny <zhenya.raewskiy@yandex.ru>
2024-03-12 14:46:51 +0300

    adding basic settings and styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import { store } from "./store/store";
+import "./main.css";
 
 const container = document.getElementById("root");
 
