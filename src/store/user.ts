36cb498aaec161e12aad5a32606406d3f86e4b13
Evgeny <zhenya.raewskiy@yandex.ru>
2024-03-20 18:13:29 +0300

    refactoring of cards and adding the 'show more' functionality

--- src/store/user.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "./store";
-
-const selectUserState = (state: RootState) => state.user;
-
-export const selectUser = createSelector(
-  [selectUserState],
-  userState => userState.user
-);
