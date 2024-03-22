08ad5d3e9c8b609ad3a2978edbbee11279902cbd
Evgeny <zhenya.raewskiy@yandex.ru>
2024-03-22 14:39:53 +0300

    creating a favorites page and favorites store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,15 @@ import { genshinApi } from "./genshinApi";
 import { userSLice } from "./slice/userSlice";
 import { authSlice } from "./slice/authSlice";
 import { moreCardSlice } from "./slice/moreCardSlice";
+import { favoriteSlice } from "./slice/favoriteSlice";
 
 export const store = configureStore({
   reducer: {
     [genshinApi.reducerPath]: genshinApi.reducer,
     user: userSLice.reducer,
     auth: authSlice.reducer,
-    moreCard: moreCardSlice.reducer
+    moreCard: moreCardSlice.reducer,
+    favorite: favoriteSlice.reducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(genshinApi.middleware)
