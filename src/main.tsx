44ae73fad058aa642e60add740940db7989c2ee2
Evgeny <zhenya.raewskiy@yandex.ru>
2024-04-01 23:52:51 +0300

    code refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "./App";
 import { store } from "./store/store";
-import "./main.css";
 import { ThemeCtx } from "./context/ThemeCtx";
+import "./main.css";
 
 const container = document.getElementById("root");
 
